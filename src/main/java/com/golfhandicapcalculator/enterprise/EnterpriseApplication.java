569daeb9bc19b608aa232bdc2520db076e86d077
Nirupama Poojari <poojarna@mail.uc.edu>
2025-10-22 20:26:12 -0400

    Added a SpringBootApplication tag to resolve compilation issues.

--- src/main/java/com/golfhandicapcalculator/enterprise/EnterpriseApplication.java
+++ src/main/java/com/golfhandicapcalculator/enterprise/EnterpriseApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+@SpringBootApplication
 public class EnterpriseApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnterpriseApplication.class, args);
