7f990e6f32d4805bbac22c17637d4e50a212f17e
21ahart <aidenhartranft@gmail.com>
2025-10-26 20:44:39 -0400

    wip

--- src/main/java/com/golfhandicapcalculator/enterprise/EnterpriseApplication.java
+++ src/main/java/com/golfhandicapcalculator/enterprise/EnterpriseApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
 public class EnterpriseApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnterpriseApplication.class, args);
